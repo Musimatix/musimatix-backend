c4cbbaab7f1f30bab2e5a3c784e8a85c215a0685
Andrey Demin <ade@exante.eu>
2016-05-21 21:42:19 +0300

    Add and test msgpack lib

--- verse-rates-rest/src/main/scala/verse/rates/Obj2Bytes.scala
+++ verse-rates-rest/src/main/scala/verse/rates/Obj2Bytes.scala
@@ -1,8 +1,28 @@
 package verse.rates
 
-/** **
-  *
-  * ***/
-class Obj2Bytes {
+import org.msgpack.annotation.Message
+import org.msgpack.ScalaMessagePack
+
+object Obj2Bytes {
+
+  @Message // Don't forget to add Message annotation.
+  class YourClass{
+    var name : String = ""
+    var age : Int = 2
+  }
+
+  def main(args: Array[String]) {
+
+    val obj = new YourClass()
+    obj.name = "hoge"
+    obj.age = 22
+    val serialized : Array[Byte] = ScalaMessagePack.write(obj)
+
+    val deserialized : YourClass = ScalaMessagePack.read[YourClass](serialized)
+
+    println(deserialized)
+    println(deserialized.name + ", " + deserialized.age)
+  }
+
 
 }
